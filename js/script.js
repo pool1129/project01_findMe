3ed3e40b54b3037512789670f9e1a51edca56fd5
pool1129 <pool1129@naver.com>
2021-08-20 17:13:22 +0900

    2021.08.20

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function follow(e){
     gsap.to(cursor, {duration: .3, left: e.pageX - circle.width/2, top: e.pageY - circle.height/2});
 }
 
-document.addEventListener("mousemove", follow);
+document.addEventListener("touchmove", follow);
 
 // TODO : 카운트 다운
 const count = document.querySelector('.count');
